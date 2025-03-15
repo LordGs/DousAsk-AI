f82a8e975f4b4f9e3fca06ac1fc279eff2ee519d
Lordvic <lrdvcgbs@gmail.com>
2025-03-15 11:49:00 +0800

    fixed ports non detected

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from sentence_transformers import SentenceTransformer, util
@@ -77,4 +78,5 @@ def ask():
     return jsonify({"answer": best_answer})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from Render, default to 5000
+    app.run(host="0.0.0.0", port=port)
